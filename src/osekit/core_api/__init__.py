0242f5143b75af2939ffcb78af12b0b47f34a13b
gmanatole <grosmartial.anatole@gmail.com>
2026-01-29 18:14:45 +0100

    add aux file manager in init

--- src/osekit/core_api/__init__.py
+++ src/osekit/core_api/__init__.py
@@ -1,3 +1,5 @@
 from osekit.audio_backend.audio_file_manager import AudioFileManager
+from osekit.auxiliary_backend.auxiliary_file_manager import AuxiliaryFileManager
 
 audio_file_manager = AudioFileManager()
+auxiliary_file_manager = AuxiliaryFileManager()
